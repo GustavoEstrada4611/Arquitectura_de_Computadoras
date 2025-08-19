15ce122c4db9328ac3a579c58e74a81b8fcf2cfb
GustavoEstrada4611 <gustavo.estrada4611@alumnos.udg.mx>
2025-08-18 18:45:51 -0600

    Add files via upload

---
+++ Practica1/Compuerta_AND.v
@@ -0,0 +1,13 @@
+//1. Definicion de modulo y sus entradas y salidas
+module _and (input A, input B, output C);
+//2. Declarar señales/elementos internos
+//NA
+//3. Comportamiento del modulo
+	//(Asignaciones, Instancias, Conexiones)
+assign C=A&B;
+
+
+
+endmodule
+
+//Repositorio GitHub: https://github.com/GustavoEstrada4611/Arquitectura_de_Computadoras
